604e20f58894fe9ed814d8cca6aaea187f960cbd
Niels Kjøller Hansen <niels.k.h@gmail.com>
2016-02-15 09:05:20 +0100

    Changed ugly html to be multiline

--- src/filterwizard-control.js
+++ src/filterwizard-control.js
@@ -1,4 +1,4 @@
-/* global L document */
+/* global L document cartodb */
 
 L.Control.FilterWizard = L.Control.extend({
   options: {
@@ -10,6 +10,7 @@ L.Control.FilterWizard = L.Control.extend({
   },
 
   initialize: function(options) {
+    this.controller = cartodb.filterWizard.controller.init(options);
     console.log(options);
   },
 
@@ -19,7 +20,15 @@ L.Control.FilterWizard = L.Control.extend({
     var modal = L.DomUtil.create('div', 'modal fade', document.body);
     modal.setAttribute('role', 'dialog');
     modal.setAttribute('id', 'filterModal');
-    modal.innerHTML = '<div class="modal-dialog"><div class="modal-content"><form name="filter_form" action=""><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h3>V&aelig;lg filtre:</h3></div><div class="modal-body"><table><tr id="filterheader"></tr><tr id="filterbody"></tr></table></div><div class="modal-footer"><div id="filtercount">Du har valgt <span class="value">0</span> projekter.</div><button type="button" class="btn btn-default btn-close" data-dismiss="modal" id="filtersubmit">Udfør</button></div></form></div></div>';
+    modal.innerHTML = '<div class="modal-dialog"><div class="modal-content">' +
+      '<form name="filter_form" action=""><div class="modal-header">' +
+      '<button type="button" class="close" data-dismiss="modal">&times;' +
+      '</button><h3>V&aelig;lg filtre:</h3></div><div class="modal-body">' +
+      '<table><tr id="filterheader"></tr><tr id="filterbody"></tr></table>' +
+      '</div><div class="modal-footer"><div id="filtercount">Du har valgt ' +
+      '<span class="value">0</span> projekter.</div><button type="button" ' +
+      'class="btn btn-default btn-close" data-dismiss="modal" ' +
+      'id="filtersubmit">Udfør</button></div></form></div></div>';
 
     var button = L.DomUtil.create('button', 'btn btn-info btn-lg', container);
     button.setAttribute('data-toggle', 'modal');
